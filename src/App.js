46cbfa18c86925c74df60b87caea67c9931f9922
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-09-21 20:04:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Lautaro Ogando
+          Lautaro Nahuel Ogando
         </p>
         <a
           className="App-link"
