6f67ed8e8325aa945039cfa80c1c744ccaa160f0
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-09-21 19:57:29 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Lautaro Ogando
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Future Systems Engineer
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
