4e3f890804247efa76237da98efa8da265b1a820
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-09-28 10:40:37 -0300

    Menu e-commerce

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Products</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
+
+
