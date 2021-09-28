75ce684254eb17ca3688d2e965270c3e1df945c1
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-09-28 11:11:12 -0300

    Menu e-commerce

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import logo from "./logo.svg";
 import "./App.css";
-
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lautaro Nahuel Ogando
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Future Systems Engineer
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
 }
