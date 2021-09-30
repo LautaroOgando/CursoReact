d104494f307cc2e8b5cf0f8e8aa4f3fbfe835c95
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-09-30 11:40:07 -0300

    Landing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
+import ItemListContainer from "./components/ItemListContainer";
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <ItemListContainer
+      greeting="Productos Destacados"
+      />
     </div>
   );
 }
