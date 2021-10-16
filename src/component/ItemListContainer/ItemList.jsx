062ec1bef4aa9c4d6c6850404230ae38ac7c7a2e
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-10-16 19:40:52 -0300

    Detalles

--- src/component/ItemListContainer/ItemList.jsx
+++ src/component/ItemListContainer/ItemList.jsx
@@ -1,4 +1,5 @@
 import { computeHeadingLevel } from '@testing-library/dom';
+import axios from 'axios';
 import React,{useEffect, useState} from 'react';
 import Item from '../Item/Item';
 
@@ -27,51 +28,52 @@ const ItemListContainer = () => {
         }
   
       }
-     
-        const date=new Promise((response,reject)=>{
-            setTimeout(()=>{ 
-                response([
-                {
-                    id:1,
-                    title:"Iphone XR",
-                    description:"64GB,4000mAh,13Mpx",
-                    price:"800 USD",
-                    pictureurl:"https://http2.mlstatic.com/D_NQ_NP_819452-MLA46669400814_072021-O.webp"
-                },
-                {
-                    id:1,
-                    title:"Iphone 8",
-                    description:"64GB,4000mAh,13Mpx",
-                    price:"500 USD",
-                    pictureurl:"https://http2.mlstatic.com/D_NQ_NP_761686-MLA31003080334_062019-O.webp"
-                }
 
-                ]);
-            },2000)
-        });
-   
-    
+      const date=new Promise((response,reject)=>{
+        setTimeout(()=>{ 
+            response([
+            {
+                id:1,
+                title:"Iphone XR",
+                description:"64GB,4000mAh,13Mpx",
+                price:"800 USD",
+                pictureurl:"https://http2.mlstatic.com/D_NQ_NP_819452-MLA46669400814_072021-O.webp"
+            },
+            {
+                id:1,
+                title:"Iphone 8",
+                description:"64GB,4000mAh,13Mpx",
+                price:"500 USD",
+                pictureurl:"https://http2.mlstatic.com/D_NQ_NP_761686-MLA31003080334_062019-O.webp"
+            }
 
-        useEffect(()=>{
-            if(!producto){
-       date.then((resp)=>{
-            console.log('ok');
-            setProducto(resp);
-       });
-    }
-    },[producto]);
+            ]);
+        },2000)
+    });
 
 
+                useEffect(()=>{
+                    if(!producto){
+               date.then((resp)=>{
+                    console.log('ok');
+                    setProducto(resp);
+               });
+            }
+            },[producto]);
+        
+                
+                console.log(producto)
 
     return (
-        <div>
-            {producto && producto.map((producto=>
+         <div>
+        {producto && producto.map((producto=>
             
-            <Item initial={initial} key={producto.id} nombre={producto.title} description={producto.description} stock={stock} onAdd={onAdd} removeAdd={removeAdd} checkStock={checkStock}  text="Iphone XR" title="1100USD" img={producto.pictureurl}/>
+            <Item initial={initial} key={producto.id} nombre={producto.title} description={producto.description} stock={stock} onAdd={onAdd} removeAdd={removeAdd} checkStock={checkStock}   img={producto.pictureurl}/>
 
             ))}
-        </div>
-    )
-}
+    </div>
+)
+
+        }
 
 export default ItemListContainer
--- src/component/Main/Main.jsx
+++ src/component/Main/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Navbar from '../Navbar/Navbar'
+import ItemDetailContainer from '../ItemDetailContainer/ItemDetailContainer'
+
 import ItemList from '../ItemListContainer/ItemList'
 
 const Main = () => {
     return (
         <div>
-            <Navbar/>
-            <ItemList/>
+            
+            <ItemDetailContainer/>
         </div>
     )
 }
