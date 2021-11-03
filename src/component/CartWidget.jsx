8fe01dfc3795714ccbca969335482d120b75a6db
Lautaro Ogando <lautaro.ogando@gmail.com>
2021-11-02 23:17:20 -0300

    Desafio 11

--- src/component/CartWidget.jsx
+++ src/component/CartWidget.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
-
-
+import { Link } from 'react-router-dom'
+import { CartContext } from './CartContext/CartContextProvider';
+import { useContext } from 'react';
 
 const CartWidget = () => {
+
+    const {cartList,initial,stock,buy,removeAdd,checkStock,increment,finish}= useContext(CartContext);
     return (
         <div>
-            
-            <i class="bi bi-cart"></i>
-     
+            {cartList && cartList.map((cartList=>
+            <Link to="/cart"><i class="bi bi-cart">{cartList.type}</i></Link>
+            ))}
         </div>
     )
 }
--- src/component/ItemListContainer/ItemList.jsx
+++ src/component/ItemListContainer/ItemList.jsx
@@ -3,16 +3,21 @@ import axios from 'axios';
 import React,{useEffect, useState} from 'react';
 import Item from '../Item/Item';
 import { useParams } from 'react-router';
-
+import { getFirestore } from '../../service/GetFireBase';
 
 
 const ItemListContainer = () => {
 
-   
+     
     const [producto, setProducto] = useState(null);
 
     const {name}=useParams();
- 
+    //const db = getFirestore();
+    //db.coleccion('Items').get()//trae toda la coleccion
+    //.then(resp=>console.log(resp));
+
+    //db.coleccion('item').doc('mY5ilcB0SHUqV9HJqsQ4').get()//trae coleccion por id
+
                const getItem= async()=>{
                 try{
                      const produc= await axios.get("https://rickandmortyapi.com/api/character");
